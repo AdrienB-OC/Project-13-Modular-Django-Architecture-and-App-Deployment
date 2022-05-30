1941b33beb58e228b873fc0842d1acccaaef03cc
Adrien Burgun <adrienburgun@hotmail.fr>
2022-05-31 04:16:03 +0900

    Trying sentry config with explicit dsn

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,8 +3,17 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
-    dsn=os.getenv('SENTRY_DSN'),
-    integrations=[DjangoIntegration()]
+    dsn="https://9ba1b183df14433bb3d8c1bbddf46d6c@o1268155.ingest.sentry.io/6455128",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
 )
 
 
